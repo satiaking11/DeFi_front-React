528a08cc03972eaf673579892ed39d48a34b5d72
satiaking11 <Santiatop01@gmail.com>
2021-08-27 22:06:18 +0300

    add version to deployer

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 npm install
 npm run build
-ssh -i ~/Desktop/Projects/Ansible/betnomi-ansible/keys/betnomi-defi.pem centos@18.191.191.231 'sudo chown -R centos:centos /var/www/*'
+git rev-parse HEAD >> ./build/version.txt
+date >> ./build/version.txt
+ssh -i ~/Desktop/Projects/Ansible/betnomi-ansible/keys/betnomi-defi.pem centos@18.191.191.231 'sudo chown -R centos:centos /var/www'
 scp -i ~/Desktop/Projects/Ansible/betnomi-ansible/keys/betnomi-defi.pem -r ./build/* centos@18.191.191.231:/var/www
-ssh -i ~/Desktop/Projects/Ansible/betnomi-ansible/keys/betnomi-defi.pem centos@18.191.191.231 'sudo chown -R nginx:nginx /var/www/*'
+ssh -i ~/Desktop/Projects/Ansible/betnomi-ansible/keys/betnomi-defi.pem centos@18.191.191.231 'sudo chown -R nginx:nginx /var/www'
